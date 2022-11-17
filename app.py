95d50764f2c75262282ef80840b2160cf6b5d76b
theiago <theiago@live.com>
2022-11-17 19:04:46 -0300

    removed cache folder

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from os import listdir
 from folders import steam_libraries
 
-for library in steam_libraries:
-    files_list = listdir(library)
-    
-    for file in files_list:
-        if file.startswith("appmanifest"):
-            path = library + "\\" + file
-            f = open(path, "r")
-            line = f.readlines()
-            if len(line) > 10:
-                print("JOGO ENCONTRADO:", line[5].split("\"")[3])
-                line[19] = '\t"AutoUpdateBehavior"\t\t"2"\n'
-                with open(path, "w") as file:
-                    file.writelines(line)
+if __name__ == "__main__":
+    for library in steam_libraries:
+        files_list = listdir(library)
+        
+        for file in files_list:
+            if file.startswith("appmanifest"):
+                path = library + "\\" + file
+                f = open(path, "r")
+                line = f.readlines()
+                if len(line) > 10:
+                    print("JOGO ENCONTRADO:", line[5].split("\"")[3])
+                    line[19] = '\t"AutoUpdateBehavior"\t\t"2"\n'
+                    with open(path, "w") as file:
+                        file.writelines(line)
