203c4bd290b6366e858628b1b17c997ed57f0898
Iago Henrique <theiago@live.com>
2023-03-03 15:24:56 -0300

    changed string concatenation to path.join / added commentaries

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from os import listdir, system
+from os import listdir, system, path
 from folders import steam_libraries
 
 if __name__ == "__main__":
@@ -7,13 +7,17 @@ if __name__ == "__main__":
         
         for file in files_list:
             if file.startswith("appmanifest"):
-                path = library + "\\" + file
-                f = open(path, "r")
-                line = f.readlines()
-                if len(line) > 10:
-                    print("GAME FOUND:", line[5].split("\"")[3])
-                    line[19] = '\t"AutoUpdateBehavior"\t\t"2"\n'
-                    with open(path, "w") as file:
-                        file.writelines(line)
+                # Join the steam library path and the game path
+                game_path = path.join(library, file)
+                with open(game_path, "r") as f:
+                    # Read lines from the appmanifest file
+                    line = f.readlines()
+                    if len(line) > 10:
+                        print("GAME FOUND:", line[5].split("\"")[3])
+                        # Change the Update Behavior to High Priority (2)
+                        line[19] = '\t"AutoUpdateBehavior"\t\t"2"\n'
+                        # Save the changes to final file
+                        with open(game_path, "w") as file:
+                            file.writelines(line)
     print("\nAll installed games have been set update to high priority.\n")
     system('pause')
