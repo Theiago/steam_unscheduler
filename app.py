58c65f6954ab08991931b25c5293819420001966
Iago Henrique <theiago@live.com>
2023-03-31 11:39:36 -0300

    refactored the code with functions

--- app.py
+++ app.py
@@ -1,23 +1,50 @@
-from os import listdir, system, path
+import os
 from folders import steam_libraries
 
-if __name__ == "__main__":
-    for library in steam_libraries:
-        files_list = listdir(library)
+
+def set_high_priority_update_behavior(appmanifest_file_path):
+    with open(appmanifest_file_path, "r+") as f:
+        lines = f.readlines()
+
+        if len(lines) <= 19:
+            # The appmanifest is missing.
+            return False
+
+        lines[19] = '\t"AutoUpdateBehavior"\t\t"2"\n'
+        f.seek(0)
+        f.writelines(lines)
+        f.truncate()
+        return True
+
+# Get a list of appmanifest file paths in the given libraries.
+def get_appmanifest_files(libraries):
+    appmanifest_files = []
+
+    for library in libraries:
+        files_list = os.listdir(library)
         
         for file in files_list:
             if file.startswith("appmanifest"):
-                # Join the steam library path and the game path
-                game_path = path.join(library, file)
-                with open(game_path, "r") as f:
-                    # Read lines from the appmanifest file
-                    line = f.readlines()
-                    if len(line) > 10:
-                        print("GAME FOUND:", line[5].split("\"")[3])
-                        # Change the Update Behavior to High Priority (2)
-                        line[19] = '\t"AutoUpdateBehavior"\t\t"2"\n'
-                        # Save the changes to final file
-                        with open(game_path, "w") as file:
-                            file.writelines(line)
-    print("\nAll installed games have been set update to high priority.\n")
-    system('pause')
+                appmanifest_files.append(os.path.join(library, file))
+
+    return appmanifest_files
+
+
+if __name__ == "__main__":
+    appmanifest_files = get_appmanifest_files(steam_libraries)
+    updated_files = []
+
+    for file_path in appmanifest_files:
+        if set_high_priority_update_behavior(file_path):
+            updated_files.append(file_path)
+
+    if updated_files:
+        print("The following games have been set to update with high priority:")
+        for file_path in updated_files:
+            with open(file_path, "r") as f:
+                game_name = f.readlines()[5].split("\"")[3]
+                print(game_name)
+    else:
+        print("No games were updated.")
+
+    input("Press Enter to continue...")
