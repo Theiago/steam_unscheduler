8e6ed39b60c8c4340d212a4819c7b2ab066321ac
Iago Henrique <theiago@live.com>
2023-03-31 11:42:24 -0300

    changed path concatenation to os.path.join and added functions

--- folders.py
+++ folders.py
@@ -1,12 +1,23 @@
 import winreg
+import os
 
-install_path = winreg.QueryValueEx(winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\WOW6432Node\Valve\Steam"), "InstallPath")[0] + "\config\libraryfolders.vdf"
 
-f = open(install_path, "r")
-steam_config = f.readlines()
-steam_libraries = []
+def get_steam_libraries():
+    # Get the Steam install path in the Windows Registers
+    steam_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\WOW6432Node\Valve\Steam")
+    install_path = os.path.join(winreg.QueryValueEx(steam_key, "InstallPath")[0], "config", "libraryfolders.vdf")
 
-for line in steam_config:
-    if line.startswith("\t\t\"path\""):
-        directory = line.split("\"")[3] + "\steamapps"
-        steam_libraries.append(directory)
+    with open(install_path, "r") as f:
+        steam_config = f.readlines()
+
+    steam_libraries = []
+
+    # Change the priority to high
+    for line in steam_config:
+        if line.startswith("\t\t\"path\""):
+            directory = line.split("\"")[3]
+            steam_libraries.append(os.path.join(directory, "steamapps"))
+
+    return steam_libraries
+
+steam_libraries = get_steam_libraries()
